2dc087d96fccdaebbb4273e17ca659de19bf4a2f
Vitalii <shketvitalik96@gmail.com>
2024-10-21 22:29:32 +0100

    GitHub

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 "use strict";
 
-let sum = 0;
-const step = 4;
-
-debugger;
-for (let i = 0; i <= step; i++){
-  sum += i;
-}
-console.log(sum);
+let sum = 'b';
+// console.log(sum.slice(0, 0));
+// console.log('hello');
+// // console.log(sum.charCodeAt());
+// let result = '';
+// for (let i = 0; i < 256; i++){
+//   result += String.fromCodePoint(i);
+// }
+//   console.log(typeof(result));
+  let url = 'exampleshop.com/dry-water-just-add-water-to-get-water-p-147-24122017.html >> productID is 147';
+  let identifier = url.indexOf('-p-'); 
+  console.log(url.slice(3));
+  
